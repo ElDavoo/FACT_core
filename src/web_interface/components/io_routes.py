5d3a4981c772fdde81cc527a682de973d9b0bfbe
Davide Palma (ElDavo) <311394@studenti.unimore.it>
2024-04-09 03:42:38 -0500

    radare: change api path

--- src/web_interface/components/io_routes.py
+++ src/web_interface/components/io_routes.py
@@ -113,7 +113,7 @@ class IORoutes(ComponentBase):
         binary, _ = result
         try:
             host = config.frontend.radare2_url
-            response = requests.post(f'{host}/v1/retrieve', data=binary, verify=False)
+            response = requests.post(f'{host}/radare-api/v1/retrieve', data=binary, verify=False)
             if response.status_code != http.HTTPStatus.OK:
                 raise TimeoutError(response.text)
             target_link = f"{host}{response.json()['endpoint']}m/"
