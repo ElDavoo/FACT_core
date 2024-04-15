41315d7afdbd899b8e7649f54db3bb467b8b788a
Davide Palma (ElDavo) <311394@studenti.unimore.it>
2024-04-15 09:09:20 -0500

    cwe_checker: rebase to alpine 3.19

--- src/plugins/analysis/qemu_exec/code/qemu_exec.py
+++ src/plugins/analysis/qemu_exec/code/qemu_exec.py
@@ -33,7 +33,7 @@ if TYPE_CHECKING:
 TIMEOUT_IN_SECONDS = 15
 EXECUTABLE = 'executable'
 EMPTY = '(no parameter)'
-DOCKER_IMAGE = 'fact/qemu-exec:alpine-3.18'
+DOCKER_IMAGE = 'fact/qemu-exec:alpine-3.19'
 QEMU_ERRORS = ['Unsupported syscall', 'Invalid ELF', 'uncaught target signal']
 CONTAINER_TARGET_PATH = '/opt/firmware_root'
 
