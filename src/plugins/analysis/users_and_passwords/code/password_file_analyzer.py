2a8577a0e4773e78fbf34a4492cb7b54041aaf7e
Davide Palma (ElDavo) <311394@studenti.unimore.it>
2024-07-04 04:14:49 -0500

    rebase components to alpine 3.20

--- src/plugins/analysis/users_and_passwords/code/password_file_analyzer.py
+++ src/plugins/analysis/users_and_passwords/code/password_file_analyzer.py
@@ -117,7 +117,7 @@ def crack_hash(passwd_entry: bytes, result_entry: dict, format_term: str = '') -
         fp.write(passwd_entry)
         fp.seek(0)
         john_process = run_docker_container(
-            'fact/john:alpine-3.19',
+            'fact/john:alpine-3.20',
             command=f'/work/input_file {format_term}',
             network_disabled=True,
             mounts=[
